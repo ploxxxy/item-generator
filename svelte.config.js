fa5a7790f8055644e35779adac74e9d3d0d4f108
ploxxxy <greenenstheo@gmail.com>
2022-05-29 22:01:22 +0300

    built website

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,13 @@
-import preprocess from "svelte-preprocess";
-import adapter from "@sveltejs/adapter-static";
+import preprocess from 'svelte-preprocess'
+import adapter from '@sveltejs/adapter-static'
+
+const dev = process.env.NODE_ENV === 'development'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
     paths: {
-      base: '/item-generator'
+      base: dev ? '' : '/item-generator'
     },
     adapter: adapter({
         fallback: 'index.html',
